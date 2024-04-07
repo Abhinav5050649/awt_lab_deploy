4de0c7c5a71eb40359ef7415ec7c22c9fcaca6de
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-04-07 20:03:14 +0530

    Modified: index.js

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    try{
-        return res.status(200).send("Hello, World");
-    } catch (error) {
-        return res.status(500).send("Internal Server Error!")
-    }
+    res.send("Hello, World!");
 });
 
 app.listen(port, () => {
